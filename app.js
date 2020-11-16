2947e9f4253005c019d4087babbb8fbb0b350f1d
Bruno Sabino <bruno.p.sabino@gmail.com>
2020-11-16 20:40:16 -0300

    Adding body parsing

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
-const app = express()
-
+const bodyParser = require('body-parser')
 const port = 3000
 
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false})) //Adicionando body parser na aplicação
+
+/* Fluxo de middlewares vai top to bottom*/
 app.get('/', (req, res) => {
-    res.send("Hello Word!")
+    res.send('<h1>Hello Express!</h1>')
 })
+
+app.use('/add-product', (req, res) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+})
+
+app.use('/product', (req, res) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
