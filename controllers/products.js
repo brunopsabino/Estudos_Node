b0be2710541141481dc5f4cf9c7971122b9a5b2d
Bruno Sabino <bruno.p.sabino@gmail.com>
2020-12-14 19:57:01 -0300

    Adding a Product Model

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,17 @@
-const products = []
+const Product = require('../models/product')
 
 exports.getAddProduct = (req, res) => {
     res.render('add-product', {docTitle: 'Add Product', path: '/admin/add-product'})
 }
 
 exports.postAddProduct = (req, res) => {
-    products.push({ title: req.body.title })
+    const product = new Product(req.body.title)
+    product.save();
     res.redirect('/')
 }
 
 exports.getProducts = (req, res) => {
+    const products = Product.fetchAll()
     //console.log(products)
     res.render('shop', {prods: products, docTitle: 'Shop', path: '/'}) //Renderizar um template. Arwuivos pug foram definidos como padrão. Pasta views, definida como pasta de templates
 }
