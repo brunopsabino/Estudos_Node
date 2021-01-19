4ea3d65239c8edf6268218752dbaa8ef61850465
Bruno Sabino <bruno.p.sabino@gmail.com>
2021-01-18 23:52:42 -0300

    Creating and Managing a Dummy User

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user
+            next()
+        })
+        .catch(err => console.log(err))
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -31,8 +40,19 @@ app.use(errorController.get404);
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
 
 sequelize
-    .sync({force: true} )
+    //.sync({force: true} )
+    .sync()
     .then(result => {
+        return User.findByPk(1)
+    })
+    .then(user => {
+        if(!user){
+          return User.create({name: 'Bruno', email: 'bruno.p.sabino@gmail.com'}) 
+        }
+        return user
+    })
+    .then(user => {
+        //console.log(user)
         app.listen(3000);
     })
     .catch(err => {
