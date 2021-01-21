bdb33f866cc5e3521fbbd31a6970171a620e8ccd
Bruno Sabino <bruno.p.sabino@gmail.com>
2021-01-20 23:44:33 -0300

    Creating and Fetching a Cart

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ Cart.belongsToMany(Product, {through: CartItem})
 Product.belongsToMany(Cart, {through: CartItem})
 
 sequelize
-    .sync({force: true} )
-    //.sync()
+    //.sync({force: true} )
+    .sync()
     .then(result => {
         return User.findByPk(1)
     })
@@ -61,6 +61,9 @@ sequelize
     })
     .then(user => {
         //console.log(user)
+        return user.createCart()
+    })
+    .then(cart => {
         app.listen(3000);
     })
     .catch(err => {
