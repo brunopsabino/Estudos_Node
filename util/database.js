ef6ae2ac2427734cc19fda72cadc133ec2c534af
Bruno Sabino <bruno.p.sabino@gmail.com>
2021-01-25 23:44:47 -0300

    Connect to MongoDB

--- util/database.js
+++ util/database.js
@@ -1,7 +1,16 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node', 'root', '1q2w3e4r', {
-    dialect: 'mysql', 
-    host: 'localhost'})
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://nodecurse:nodecurse@cluster0.xohc1.mongodb.net/test?retryWrites=true&w=majority')
+    .then(client => {
+        console.log('Connected!')
+        callback(client)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
 
-module.exports = sequelize
+
+module.exports = mongoConnect
