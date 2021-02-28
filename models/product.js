b84ca91db4946f4b0f8ca1e64415bb4e7aa84174
Bruno Sabino <bruno.p.sabino@gmail.com>
2021-02-28 18:14:23 -0300

    Fetching a Single Product

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb')
 const getDb = require('../util/database').getDb
 
 class Product {
@@ -32,6 +33,22 @@ class Product {
         .catch(err => {
           console.log(err)
         })
+  }
+
+
+  static findbyId(prodID) {
+    const db = getDb()
+    return db
+      .collection('products')
+      .find({ _id: new mongodb.ObjectID(prodID)})
+      .next()
+      .then(product => {
+        console.log(product)
+        return product
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
 
